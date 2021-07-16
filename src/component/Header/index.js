bb590dc948b50eb6fc9439f24230d19b4cb61684
Jennifer <jennifer861025@gmail.com>
2021-07-16 19:05:20 +0800

    feat: Header alert from component

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useContext } from "react";
 import "../../select.scss";
 import styles from "./styles.module.scss";
 import logo from "../../assets/image/logo.png";
+import Alert from "../../component/Alert";
 
 //uiStore
 import { UIStoreContext } from "../../uiStore/reducer";
@@ -25,6 +26,11 @@ const Header = () => {
   // const courseDataID = "60de73cfe4480444509e1b2e";
   // const classroomDataID="60dd2a3d9b567c224c85482c";
 
+  //Alert的開關及內容
+  const [Alertshow, setAlertshow] = useState(false);
+  const [Alerttext, setAlerttext] = useState("");
+
+  //新增課程相關
   const [addingCourse, isAddingCourse] = useState(false);
   const [newCourseName, setNewCourseName] = useState("");
 
@@ -58,15 +64,18 @@ const Header = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [courseWeeksDataLoading]);
 
+  //切換老師課程
   const courseSelectHandler = (course) => {
     setCourseDataID(uiDispatch, course);
     getCourseWeeksData(dispatch, { courseDataID: course });
   };
 
+  //點選新增課程按鈕
   const addCourseHandler = () => {
     isAddingCourse(true);
   };
 
+  //新增課程確定按鈕
   const addCourseSubmitHandler = () => {
     if (newCourseName !== "") {
       addCourse({
@@ -77,10 +86,12 @@ const Header = () => {
       isAddingCourse(false);
       setNewCourseName("");
     } else {
-      alert("請輸入課程名稱！");
+      setAlertshow(true);
+      setAlerttext("請輸入新增之課程名稱！");
     }
   };
 
+  //新增課程取消按鈕
   const addCourseCancelHandler = () => {
     isAddingCourse(false);
   };
@@ -90,72 +101,79 @@ const Header = () => {
   }, [newCourseName]);
 
   return (
-    <header className={styles.header}>
-      <div className={styles.leftSide}>
-        <div className={styles.leftSide_logo}>
-          <img src={logo} />
-        </div>
-        <div className={styles.leftSide_title}>課堂監控後台</div>
-        <select
-          className={styles.leftSide_select}
-          onChange={(e) => courseSelectHandler(e.target.value)}
-        >
-          {courses.map((course) => (
-            <option
-              value={course.courseDataID}
-              key={course.courseDataID}
-              selected={
-                course.courseDataID === courseDataIDState ? "selected" : ""
-              }
+    <>
+    <Alert
+        show={Alertshow}
+        onHide={() => setAlertshow(false)}
+        text={Alerttext}
+      />
+      <header className={styles.header}>
+        <div className={styles.leftSide}>
+          <div className={styles.leftSide_logo}>
+            <img src={logo} />
+          </div>
+          <div className={styles.leftSide_title}>課堂監控後台</div>
+          <select
+            className={styles.leftSide_select}
+            onChange={(e) => courseSelectHandler(e.target.value)}
+          >
+            {courses.map((course) => (
+              <option
+                value={course.courseDataID}
+                key={course.courseDataID}
+                selected={
+                  course.courseDataID === courseDataIDState ? "selected" : ""
+                }
+              >
+                {course.courseName}
+              </option>
+            ))}
+          </select>
+          <button
+            className={
+              addingCourse
+                ? `${styles.leftSide_button} ${styles.leftSide_button__none}`
+                : `${styles.leftSide_button}`
+            }
+            onClick={addCourseHandler}
+          >
+            新增課程
+          </button>
+          <form
+            className={
+              addingCourse
+                ? `${styles.leftSide_form}`
+                : `${styles.leftSide_form} ${styles.leftSide_form__none}`
+            }
+          >
+            <input
+              id="addCourseInput"
+              type="text"
+              placeholder="請輸入課程名稱"
+              onChange={(e) => setNewCourseName(e.target.value)}
+              className={styles.leftSide_textInput}
+            ></input>
+            <div
+              onClick={addCourseSubmitHandler}
+              className={`${styles.leftSide_formBtn} ${styles.leftSide_submitBtn}`}
             >
-              {course.courseName}
-            </option>
-          ))}
-        </select>
-        <button
-          className={
-            addingCourse
-              ? `${styles.leftSide_button} ${styles.leftSide_button__none}`
-              : `${styles.leftSide_button}`
-          }
-          onClick={addCourseHandler}
-        >
-          新增課程
-        </button>
-        <form
-          className={
-            addingCourse
-              ? `${styles.leftSide_form}`
-              : `${styles.leftSide_form} ${styles.leftSide_form__none}`
-          }
-        >
-          <input
-            id="addCourseInput"
-            type="text"
-            placeholder="請輸入課程名稱"
-            onChange={(e) => setNewCourseName(e.target.value)}
-            className={styles.leftSide_textInput}
-          ></input>
+              確定
+            </div>
+            <div
+              onClick={addCourseCancelHandler}
+              className={`${styles.leftSide_formBtn} ${styles.leftSide_cancelBtn}`}
+            >
+              取消
+            </div>
+          </form>
+        </div>
+        <div className={styles.rightSide}>
           <div
-            onClick={addCourseSubmitHandler}
-            className={`${styles.leftSide_formBtn} ${styles.leftSide_submitBtn}`}
-          >
-            確定
-          </div>
-          <div
-            onClick={addCourseCancelHandler}
-            className={`${styles.leftSide_formBtn} ${styles.leftSide_cancelBtn}`}
-          >
-            取消
-          </div>
-        </form>
-      </div>
-      <div className={styles.rightSide}>
-        <div
-          className={styles.rightSide_title}
-        >{`你好，${teacherName} 老師`}</div>
-      </div>
-    </header>
+            className={styles.rightSide_title}
+          >{`你好，${teacherName} 老師`}</div>
+        </div>
+      </header>
+    </>
   );
 };
 
